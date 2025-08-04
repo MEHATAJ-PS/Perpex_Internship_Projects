f8c3d1457da07e302377154715f1a5e4ce83f404
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-04 16:41:15 +0530

    18329

--- products/views.py
+++ products/views.py
@@ -6,11 +6,10 @@ from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
 
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
 
-# Create your views here.
+#-----------------------------
+# Dynamic Menu (from database)
+#-----------------------------
 class ItemView(APIView):
 
     def get(self, request):
@@ -24,3 +23,28 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#-------------------------
+# Static Menu (hardcoded)
+#-------------------------
+class MenuAPIView(APIView):
+    def get(self.request):
+        menu = [
+            {
+                "name": "Veg Biryani",
+                "description": "Spicy rice with vegetables",
+                "price": 120.00
+            },
+            {
+                "name": "Butter Naan",
+                "description": "Tandoori naan with butter",
+                "price": 40.00
+            },
+            {
+                "name": "Paneer Tikka",
+                "description": "Grilled cottage cheese cubes",
+                "price": 150.00
+            }
+        ]
+        return Response(menu, status=status.HTTP_200_OK)
