9565091c2e8d5533590fcd342c4ce0bf61c4f502
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-04 17:24:41 +0530

    18329

--- products/views.py
+++ products/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -43,34 +41,46 @@ class MenuAPIView(APIView):
         """
         menu_data = [
             {
+
                 "name": "Veg Biryani",
                 "description": "Spicy rice with vegetables",
                 "price": 120.00
+
             },
             {
+
                 "name": "Butter Naan",
                 "description": "Tandoori naan with butter",
                 "price": 40.00
+
             },
             {
+
                 "name": "Paneer Tikka",
                 "description": "Grilled cottage cheese cubes",
                 "price": 150.00
+
             },
             {
+
                 "name": "Margherita Pizza",
                 "description": "Classic pizza with tomata sauce and mozzarella cheese.",
                 "price": 299.00
+
             },
             {
+
                 "name": "Pasta Alfredo",
                 "description": "Creamy pasta with rich Alfredo sauce and mushrooms.",
                 "price": 349.00
+
             },
             {
+
                 "name": "Caesar Salad",
                 "description": "Fresh romaine lettuce with Caesar dressing and croutons.",
                 "price": 199.00
+                
             }
         ]
-        return Response(menu, status=status.HTTP_200_OK)
+        return Response(menu_data, status=status.HTTP_200_OK)
