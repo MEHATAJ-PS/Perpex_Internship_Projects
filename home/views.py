00207e67bbd286f52960c226652297aae38f3dd3
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-05 15:59:18 +0530

    18326

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def menu_view(request):
+    
+    api_url = 'http://127.0.0.1:8000/api/products/menu/'
+
+    try:
+        response = requests.get(api_url)
+        menu_items = response.json()
+    except requests.exceptions.RequestException:
+        menu_items = [] 
+    
+    return render(request, 'home/menu.html', {'menu': menu_items})
+
