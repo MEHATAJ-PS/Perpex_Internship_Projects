a87685860b25a31028d0dca3173343edb35dbd92
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-10 15:11:10 +0530

    26056

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from .menu_views import SimpleMenuView 
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/', MenuAPIView.as_view(), name='menu-list'),
+    path('simple-menu/', SimpleMenuView.as_view(), name='simple_menu'),
 ]
