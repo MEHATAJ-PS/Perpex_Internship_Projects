a594044bd428a2480efa0f4050791d3a4c80c730
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-10 11:22:52 +0530

    26055

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import RestaurantInfo
 
-admin.site.register(RestaurantInfo)
+@admin.register(RestaurantInfo)
+class RestaurantInfoAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for RestaurantInfo model.
+    """
+    list_display = ('name',) # Columns shown in admin list view
+    search_fields = ('name',) # Search bar for restaurant names
