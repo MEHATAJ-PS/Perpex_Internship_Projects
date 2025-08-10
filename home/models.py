40e0818c37a9f5420d9e3af982f9f4e3299f3515
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-10 10:23:44 +0530

    26055

--- home/models.py
+++ home/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 class RestaurantInfo(models.Model):
-    name = models.CharField(max_length=200)
+    """
+    Stores basic restaurant information.
+    """
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Restaurant Name",
+        help_text="Enter the name of the restaurant."
+    )
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Restaurant Information"
+        verbose_name_plural = "Restaurant Information"
+        ordering = ['name']
