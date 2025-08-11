952b4aab62ba700612b41677430d5a7689a316bb
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-11 22:16:13 +0530

    29290

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('menu/', views.menu_view, name='menu'),
     path('about/', views.about_view, name='about'), 
-    path('contact/', views.contact_view, name='contact')
+    path('contact/', views.contact_view, name='contact'),
     path('reservations/', views.reservations_view, name='reservations'), 
 ]
