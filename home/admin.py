73269fc732fa5efd411f52a7af61b9b0a24973b8
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-15 15:24:42 +0530

    33007

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RestaurantInfo
+from .models import RestaurantInfo, Feedback
 
 @admin.register(RestaurantInfo)
 class RestaurantInfoAdmin(admin.ModelAdmin):
@@ -8,3 +8,14 @@ class RestaurantInfoAdmin(admin.ModelAdmin):
     """
     list_display = ('name',) # Columns shown in admin list view
     search_fields = ('name',) # Search bar for restaurant names
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Feedback model.
+    """
+    list_display = ('name', 'email', 'message', 'created_at') #Columns shown in list view
+    search_fields = ('name', 'email', 'message') # Enable search in admin
+    list_filter = ('created_at',) # Sidebar filter for date
+    ordering = ('-created_at',) # Most recent first
