31f95bb231659a4b1d232072e9f7a7f08b1d3c57
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-16 10:01:07 +0530

    33007

--- home/admin.py
+++ home/admin.py
@@ -15,7 +15,7 @@ class FeedbackAdmin(admin.ModelAdmin):
     """
     Admin configuration for Feedback model.
     """
-    list_display = ('name', 'email', 'message', 'created_at') #Columns shown in list view
-    search_fields = ('name', 'email', 'message') # Enable search in admin
-    list_filter = ('created_at',) # Sidebar filter for date
-    ordering = ('-created_at',) # Most recent first
+    list_display = ('name', 'email', 'comments', 'submitted_at') #Columns shown in list view
+    search_fields = ('name', 'email', 'comments') # Enable search in admin
+    list_filter = ('submitted_at',) # Sidebar filter for date
+    ordering = ('-submitted_at',) # Most recent first
