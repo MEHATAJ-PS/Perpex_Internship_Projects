f72ceec0d1d4cabaa7e9cf55b8c286a53fec266d
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-18 11:00:07 +0530

    34255

--- products/models.py
+++ products/models.py
@@ -3,7 +3,14 @@ from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, help_text="Description of the menu item")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.item_name)
+
+
+    class Meta:
+        ordering = ['item_name']
+        verbose_name = 'Menu Item'
+        verbose_name_plural = 'Menu Items'
