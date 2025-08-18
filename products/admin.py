4c66784b83a99e7f2497278409bad0b1e7de6402
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-18 11:11:21 +0530

    34255

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,12 @@ from .models import Item
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
+    list_display = ['item_name', 'item_price', 'created_at']
+
     search_fields = ['item_name', 'description']
+
     list_filter = ['created_at']
+    
     ordering = ['item_name']
 
 admin.site.register(Item, ItemAdmin)
