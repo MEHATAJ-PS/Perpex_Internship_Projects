e5b7a865967bbe0583a6da4ba8ad6d8fe4bd9b1b
MEHATAJ-PS <mehataj73@gmail.com>
2025-08-19 17:11:45 +0530

    35843

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import RestaurantInfo, Feedback
+from .models import RestaurantInfo, Feedback, Contact, RestaurantLocation
 
 @admin.register(RestaurantInfo)
 class RestaurantInfoAdmin(admin.ModelAdmin):
     """
     Admin configuration for RestaurantInfo model.
     """
-    list_display = ('name',) # Columns shown in admin list view
+    list_display = ('name', 'phone_number') # Columns shown in admin list view
     search_fields = ('name',) # Search bar for restaurant names
 
 
@@ -19,3 +19,25 @@ class FeedbackAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'comments') # Enable search in admin
     list_filter = ('submitted_at',) # Sidebar filter for date
     ordering = ('-submitted_at',) # Most recent first
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Contact model.
+    """
+    list_display = ('name', 'email', 'submitted_at')
+    search_fields = ('name', 'email')
+    list_filter = ('submitted_at',)
+    ordering = ('-submitted_at',)
+
+
+@admin.register(RestaurantLocation)
+class RestaurantLocationAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for RestaurantLocation model.
+    """
+    list_display = ('address', 'city', 'state', 'pincode')
+    search_fields = ('address', 'city', 'state', 'pincode')
+    list_filter = ('city', 'state')
+    ordering = ('city',)
