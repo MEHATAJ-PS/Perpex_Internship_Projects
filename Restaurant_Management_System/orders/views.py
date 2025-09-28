dbff6d364a0ddf8753563677a3a57dcad799784b
MEHATAJ-PS <mehataj73@gmail.com>
2025-09-28 19:27:02 +0530

    Update views.py

--- Restaurant_Management_System/orders/views.py
+++ Restaurant_Management_System/orders/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from .models import Order
 from .serializers import OrderSerializer
 
@@ -10,5 +12,12 @@ class OrderHistoryView(APIView):
 
     def get(self, request, format=None):
         orders = Order.objects.filter(customer=request.user).order_by("-created_at")
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
+
+        # Add pagination
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  # adjust as needed
+        paginated_orders = paginator.paginate_queryset(orders, request)
+
+        serializer = OrderSerializer(paginated_orders, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
