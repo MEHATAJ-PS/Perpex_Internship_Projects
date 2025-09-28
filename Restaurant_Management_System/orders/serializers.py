41fd586306a16ea99bd08c74d4c7232925c80ecf
MEHATAJ-PS <mehataj73@gmail.com>
2025-09-28 19:22:54 +0530

    Update serializers.py

--- Restaurant_Management_System/orders/serializers.py
+++ Restaurant_Management_System/orders/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderItem
 from products.models import Item
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ["id", "name", "price"]  # adjust fields to match your Item model
+        fields = ["id", "name", "price"]  # adjust fields to your Item model
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item = ItemSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ["id", "item", "quantity", "price"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    order_items = ItemSerializer(many=True, read_only=True)
+    order_items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = ["id", "created_at", "total_amount", "status", "order_items"]
+
